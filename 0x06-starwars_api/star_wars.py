fe52b36f08c3e5f907d59e50caea9caf9c477a85
BLDhound <lekarrikaila567@gmail.com>
2023-08-12 08:42:03 -0700

    star wars xters

---
+++ 0x06-starwars_api/star_wars.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+import json
+
+x = 0
+def jprint(obj):
+    '''
+    Create a formatted string of the python Json object
+    '''
+    text = json.dumps(obj, sort_keys=True, indent=2)
+    print(text)
+
+parameters={
+    'count': 7
+}
+
+response = requests.get('https://swapi-api.hbtn.io/api/films/')
+print(response.status_code)
+names = response.json()['results'][0]['characters']
+print(names)
